690c89d6e32966e3ae8521908469383ea197ebb4
זיידע נחמה <Hand4d13@mbjcomp.org.il>
2024-11-04 22:49:28 +0200

    Initial code+welcome functions

---
+++ Stage0/Program3504.cs
@@ -0,0 +1,17 @@
+﻿partial class Program
+{
+    private static void Main(string[] args)
+    {
+        welcome3504();
+        welcome8149();
+    }
+
+    private static void welcome3504()
+    {
+        Console.WriteLine("Enter your name:");
+        string name = Console.ReadLine();
+        Console.WriteLine("{0},welcome to my first console application", name);
+    }
+
+    static partial void welcome8149();
+}
