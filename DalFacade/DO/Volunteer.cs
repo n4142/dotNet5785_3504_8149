8c7438d334c52feb454977befaed217643c1d14c
זיידע נחמה <Hand4d13@mbjcomp.org.il>
2024-11-25 00:50:50 +0200

    התחלנו קצת פרק 1

---
+++ DalFacade/DO/Volunteer.cs
@@ -0,0 +1,13 @@
+﻿namespace DO;
+
+public record class Volunteer
+{
+    public int Id {  get; set; }
+    public string Name { get; set; }
+    public string PhonNumber {  get; set; }
+    public string Email { get; set; }
+    public string Password { get; set; }
+    public string Adress { get; set; }
+    public double latitude { get; set; }
+
+}
