78cbe0aabc1f815b507ac6e80f060615b1d9bd0b
User <User@DESKTOP-4G5A9E>
2025-06-30 15:03:05 +0300

    עי

---
+++ PL/CallWindow/CallListWindow.xaml.cs
@@ -0,0 +1,55 @@
+﻿using BlApi;
+using BO;
+using PL.Call; // אם SingleCallWindow נמצא שם
+using System;
+using System.Collections.ObjectModel;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace PL.Call
+{
+    public partial class CallListWindow : Window
+    {
+        private readonly IBl bl = Factory.Get();
+        private ObservableCollection<CallInList> callList;
+
+        public CallListWindow()
+        {
+            InitializeComponent();
+            bl.Call.AddObserver(UpdateList); // תבנית observer
+            LoadData();
+        }
+
+        private void LoadData()
+        {
+            try
+            {
+                callList = new ObservableCollection<CallInList>(bl.Call.GetCallList());
+                CallDataGrid.ItemsSource = callList;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Failed to load calls: " + ex.Message, "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+        private void UpdateList()
+        {
+            Dispatcher.Invoke(() =>
+            {
+                callList.Clear();
+                foreach (var item in bl.Call.GetCallList())
+                    callList.Add(item);
+            });
+        }
+
+        private void CallDataGrid_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            if (CallDataGrid.SelectedItem is CallInList selected)
+            {
+               // new SingleCallWindow(selected.CallId).ShowDialog();
+            }
+        }
+    }
+}
