01282ef85318f07460b7aa8e73601cbfc157b837
User <User@DESKTOP-4G5A9E>
2025-07-02 11:30:13 +0300

    pl adding simulator not finished

---
+++ PL/Converters/BoolToSimulatorButtonTextConverter.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace PL.Converters
+{
+    public class BoolToSimulatorButtonTextConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+            => (bool)value ? "stop simulator" : "start simulator";
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+            => throw new NotImplementedException();
+    }
+}
